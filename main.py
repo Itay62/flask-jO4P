ddb880c5d5379859c53f090f1db864ee21e7533e
Itay-Jacobson <itay.jacobson@post.runi.ac.il>
2023-07-20 20:24:48 +0300

    Flask update

--- main.py
+++ main.py
@@ -1,13 +1,101 @@
-from flask import Flask, jsonify
 import os
+from flask import Flask, abort
+import mysql.connector as mysql
+# from settings import dbpwd
+import json
+from flask_socketio import SocketIO
+from flask_cors import CORS
 
-app = Flask(__name__)
+
+# db = mysql.connect(
+# host = "127.0.0.1",
+# user = "root",
+# passwd = dbpwd,
+# database = "moveo")
+
+# db = mysql.connect(
+#     host="containers-us-west-83.railway.app",
+#     user="root",
+#     passwd="MbWhhASn1kFtyKrjYSlS",
+#     database="railway")
 
 
-@app.route('/')
+db = mysql.connect(
+    host=os.environ['containers-us-west-83.railway.app'],
+    user=os.environ['root'],
+    passwd=os.environ['MbWhhASn1kFtyKrjYSlS'],
+    database=os.environ['railway']
+)
+
+# For build:
+app = Flask(__name__,
+            static_folder='C:/Users/itay6/HaToaar/Moveo-Task/moveo/build',
+            static_url_path='/')
+
+
+@ app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return app.send_static_file('index.html')
+
+
+# app = Flask(__name__)
+
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@ app.route('/codelinks', methods=['GET'])
+def get_links():
+    query = "SELECT id, title from codeblocks"
+    cursor = db.cursor()
+    cursor.execute(query)
+    records = cursor.fetchall()
+    if not records:
+        abort(401)
+    cursor.close()
+    headers = ['id', 'title']
+    data = []
+    for r in records:
+        data.append(dict(zip(headers, r)))
+    return json.dumps(data)
+
+
+@ app.route('/codeblock/<int:id>', methods=['GET'])
+def get_code(id):
+    query = "SELECT id, title, code, code_solution FROM codeblocks WHERE id = %s"
+    values = (id,)
+    cursor = db.cursor()
+    cursor.execute(query, values)
+    record = cursor.fetchone()
+    if not record:
+        abort(401)
+    cursor.close()
+    headers = ['id', 'title', 'code', 'code_solution']
+    return json.dumps(dict(zip(headers, record)))
+
+
+@ socketio.on('connect')
+def handle_connect():
+    print("client connected")
+
+
+@ socketio.on('code_change')
+def handle_code_change(data):
+    print("change io")
+    id = data['id']
+    newCode = data['code']
+    query = "UPDATE codeblocks SET code = %s WHERE id = %s"
+    values = (newCode, id)
+    cursor = db.cursor()
+    cursor.execute(query, values)
+    db.commit()
+    cursor.close()
+    socketio.emit('code_updated', data)
+
+
+if __name__ == "__main__":
+    socketio.run(app)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(port=os.getenv("PORT", default=5000))
