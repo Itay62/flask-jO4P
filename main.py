c992fc2d9877342a259dfc1c5f797755ae493362
Itay-Jacobson <itay.jacobson@post.runi.ac.il>
2023-08-02 16:13:01 +0300

    added worker

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ def handle_code_change(data):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    socketio.run(app, port=os.getenv("PORT", default=5000))
